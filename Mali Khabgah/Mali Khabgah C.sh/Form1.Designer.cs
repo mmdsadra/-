2454cbee2efeb3ae477a16487439630d4d7b1347
Sadra <88672385+mmdsadra@users.noreply.github.com>
2024-07-08 18:42:22 +0330

    Add files via upload

---
+++ Mali Khabgah/Mali Khabgah C.sh/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Mali_Khabgah_C.sh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkboxes = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reasons = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.savebox = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "سینا";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 70);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "صدرا";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 100);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(78, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "مسعود";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 130);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(67, 24);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "مهدی";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 160);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(67, 24);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "یونس";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "سیو";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SaveData);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "پول";
+            // 
+            // checkboxes
+            // 
+            this.checkboxes.CheckOnClick = true;
+            this.checkboxes.FormattingEnabled = true;
+            this.checkboxes.Items.AddRange(new object[] {
+            "سینا",
+            "صدرا",
+            "مسعود",
+            "مهدی",
+            "یونس"});
+            this.checkboxes.Location = new System.Drawing.Point(20, 98);
+            this.checkboxes.Name = "checkboxes";
+            this.checkboxes.Size = new System.Drawing.Size(150, 114);
+            this.checkboxes.Sorted = true;
+            this.checkboxes.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "خریدار";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.reasons);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.checkboxes);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 319);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "اضافه";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "دلیل:";
+            // 
+            // reasons
+            // 
+            this.reasons.Location = new System.Drawing.Point(20, 238);
+            this.reasons.Name = "reasons";
+            this.reasons.Size = new System.Drawing.Size(293, 27);
+            this.reasons.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.checkBox5);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Location = new System.Drawing.Point(176, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(137, 190);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "مصرف کنندگان";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.savebox);
+            this.groupBox3.Location = new System.Drawing.Point(337, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(476, 560);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "لیست سیو شده";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 525);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "حذف";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // savebox
+            // 
+            this.savebox.FormattingEnabled = true;
+            this.savebox.ItemHeight = 20;
+            this.savebox.Location = new System.Drawing.Point(16, 28);
+            this.savebox.Name = "savebox";
+            this.savebox.ScrollAlwaysVisible = true;
+            this.savebox.Size = new System.Drawing.Size(454, 484);
+            this.savebox.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 543);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "حساب";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox1);
+            this.groupBox4.Location = new System.Drawing.Point(813, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(510, 560);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "حساب";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(498, 526);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 584);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "حساب خوابگاه";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private CheckedListBox checkboxes;
+        private Label label2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private ListBox savebox;
+        private Button button2;
+        private Label label3;
+        private TextBox reasons;
+        private Button button3;
+        private GroupBox groupBox4;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ Mali Khabgah/Mali Khabgah C.sh/Form1.cs
@@ -0,0 +1,280 @@
+namespace Mali_Khabgah_C.sh
+{
+    public partial class Form1 : Form
+    {
+        string[] names = { "sina", "sadra", "masood", "mehdi", "younes"};
+        string pathsave = @"D:\Users\Zeus\Desktop\daneshgah\Save money datas.txt";
+        struct Payment
+        {
+           
+            int payerindex;
+            List<int> users = new();
+            float payedmoney;
+            bool isfull;
+            public Payment()
+            {
+                payerindex = 0;
+                payedmoney = 0.0f;
+                isfull = false;
+                for (int i = 0; i < 5; i++)
+                {
+                    users[i] = 0;
+                }
+            }
+
+            public void setpayer(int index)
+            {
+                payerindex = index;
+            }
+
+            public int getpayer()
+            {
+                return payerindex;
+            }
+            public void setmoney(float index)
+            {
+                payedmoney = index;
+            }
+
+            public float getmoney()
+            {
+                return payedmoney;
+            }
+
+            public void setusers(params int[] ar)
+            {               
+                users = ar.ToList();                
+            }
+
+            public List<int> getusersar()
+            {
+                return users;
+                
+            }
+
+            public void setisfull(bool b)
+            {
+                isfull = b;
+            }
+
+            public bool getisfull()
+            {
+                return isfull;
+            }
+        }
+        Payment[] payments = new Payment[1000];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(pathsave))
+            {
+                string[] readfile = File.ReadAllLines(pathsave);
+                int i = 0;
+                foreach(string line in readfile)
+                {
+                    string str = line;
+                    payments[i].setpayer(int.Parse(str.Split(",")[0]));
+                    payments[i].setmoney(int.Parse(str.Split(",")[1]));
+                    int maxim = int.Parse(str.Split(",")[2]);
+                    int[] inuse = new int[5];
+                    for (int j = 1; j <= maxim; j++)
+                    {
+                        inuse[int.Parse(str.Split(",")[j + 2])] = 1;
+                    }
+                    //textBox2.Text = (str.Split(",")[1 + 2]);
+                    payments[i].setusers(inuse);
+                    payments[i].setisfull(true);
+                    string making = names[payments[i].getpayer()] + ":" + payments[i].getmoney().ToString() + ":";
+                    
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (inuse[j] == 1)
+                        {
+                            making += names[j] + ":";
+                        }
+                        
+                    }
+                    making += str.Split(",")[2 + maxim + 1].ToString();
+                    savebox.Items.Add(making);
+                    i++;
+                }
+            }
+            else
+            {
+                Close();
+            }
+
+        }
+
+        public void loader()
+        {
+
+            for (int j = 0;j < 1000; j++)
+            {
+                payments[j].setisfull(false);
+            }
+            string[] readfile = File.ReadAllLines(pathsave);
+            int i = 0;
+            foreach (string line in readfile)
+            {
+                string str = line;
+                payments[i].setpayer(int.Parse(str.Split(",")[0]));
+                payments[i].setmoney(int.Parse(str.Split(",")[1]));
+                int maxim = int.Parse(str.Split(",")[2]);
+                int[] inuse = new int[5];
+                for (int j = 1; j <= maxim; j++)
+                {
+                    inuse[int.Parse(str.Split(",")[j + 2])] = 1;
+                }
+                
+                payments[i].setusers(inuse);
+                payments[i].setisfull(true);
+                i++;
+            }
+
+        }
+        private void SaveData(object sender, EventArgs e)
+        {
+            //in:
+            float money = float.Parse(textBox1.Text);
+            bool[] checkedpeople = new bool[5];
+            checkedpeople[0] = checkBox1.Checked;
+            checkedpeople[1] = checkBox2.Checked;
+            checkedpeople[2] = checkBox3.Checked;
+            checkedpeople[3] = checkBox4.Checked;
+            checkedpeople[4] = checkBox5.Checked;
+            int numberofpayer = -1;
+            int numberofusers = 0;
+            int[] usersindex = new int[5];
+            string users = "";
+            string reasonsin = reasons.Text;
+            for (int i = 0; i < 5; i++)
+            {
+                if (checkboxes.CheckedIndices.IndexOf(i) == 0)
+                    numberofpayer = i;
+                if (checkedpeople[i])
+                {
+                    numberofusers++;
+                    users += ":" + names[i];
+                    usersindex[i] = 1;
+                }
+            }
+            users += ":" + reasonsin;
+            //save:
+            
+            File.AppendAllText(pathsave, numberofpayer.ToString() + ",");
+            File.AppendAllText(pathsave, money.ToString() + ",");
+            File.AppendAllText(pathsave, numberofusers.ToString() + ",");
+            for (int i = 0; i < 5; i++)
+            {
+                if (usersindex[i] == 1)
+                {
+                    File.AppendAllText(pathsave, i.ToString() + ",");
+                }
+              
+            }
+            File.AppendAllText(pathsave, reasonsin);
+            File.AppendAllText(pathsave, "\n");
+            savebox.Items.Add(names[numberofpayer] + ":" + money.ToString() + users);
+
+            //reset:
+            textBox1.Text = "0";
+            reasons.Text = "";
+            for (int i = 0; i < 5; i++)
+            {
+                checkboxes.SetItemChecked(i, false);
+            }
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+
+            loader();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(pathsave, "");
+            savebox.Items.Clear();
+            loader();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float[] indade = new float[5];
+            float[,] outdade = new float[5,5];
+            for (int i = 0; i < 1000; i++)
+            {
+                int dade = payments[i].getpayer();
+                float pool = payments[i].getmoney();
+                List<int> gerefte = payments[i].getusersar();
+                int userscounter = 0;
+                //sadrasadar
+                if (!payments[i].getisfull())
+                    break;
+
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (gerefte[j] == 1)
+                        userscounter++;
+                }
+               
+                indade[dade] += pool;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (gerefte[j] == 1)
+                    {
+                        outdade[dade, j] += pool / userscounter;
+                    }
+                }
+            }
+
+            for (int j = 0; j < 5; j++)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    richTextBox1.Text += outdade[j, k].ToString() + "\t";
+                }
+                richTextBox1.Text += "\n";
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (outdade[j,i] - outdade[i,j] > 0)
+                    {
+                        richTextBox1.Text += names[i].ToString() + " bede be " + names[j].ToString() + " " + (outdade[j, i] - outdade[i, j]).ToString() + " tooman \t";
+                    }
+                }richTextBox1.Text += "\n";
+            }
+            
+        }
+
+        private void checkboxes_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int x = 0;
+            for (int i = 0; i < checkboxes.Items.Count; i++)
+            {
+                checkboxes.SetItemChecked(i, sender.ToString() == checkboxes.Items[i].ToString());
+                if (sender == checkboxes.Items[i])
+                {
+                    x = i;
+                }
+            }
+            checkboxes.SetItemChecked(x, true);
+        }
+    }
+}
